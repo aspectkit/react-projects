7aed8b50a96f1f413986e2847f967edbe7964914
kit <dhinkit@gmail.com>
2024-02-12 08:57:40 -0700

    first commit

---
+++ src/components/accordian/data.js
@@ -0,0 +1,19 @@
+const data = [
+    {
+        id: '1',
+        question: "do you fear the eyes of the far shore?",
+        answer: 'No'
+    },
+    {
+        id: '2',
+        question: "do you know the golden path?",
+        answer: 'Yes'
+    },
+    {
+        id: '3',
+        question: "What is the holy cross?",
+        answer: 'The Dpad'
+    }
+];
+
+export default data;
